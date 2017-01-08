e15ef118c00ec4817c60d4a08c40dd286a2b48ef
Burhan Noman <mburhannoman@gmail.com>
2017-01-08 08:46:41 +0500

    Add the portfolio pages, implemented the browser caching and fixed the bugs related to detail pages. Included templating

--- js/main.js
+++ js/main.js
@@ -2,102 +2,144 @@
  * Created by Burhan Noman on 12/28/2016.
  */
 
-$('[data-link]').click(function(event) {
-    console.log("Redirection");
-    var dest = $(this).data('destination');
-    var source = $(this).data('source');
+$(document).ready(function() {
+    var totalPages = $("#fullpage .section.portfolio-item").length;
+    $(".page-number .total-pages").html(totalPages);
+    $('#fullpage').fullpage({
+        scrollingSpeed: 900,
+        loopTop: true,
+        loopBottom: true,
+        onLeave: function(index, nextIndex, direction) {
+            $(".page-number .current-page").html(nextIndex);
+        }
+    });
 
-    //Set the body class
-    $("body").removeClass(source + "-body").addClass(dest + "-body");
+    $("#download-resume").click(function(){
+        window.open('docs/Burhan-Design.pdf');
+    });
 
-    dest = "." + dest;
-    source = "." + source;
-    var destMeta = dest + "-meta";
-    var sourceMeta = source + "-meta";
+    $('[data-link]').click(function(event) {
+        console.log("Redirection");
+        var dest = $(this).data('destination');
+        var source = $(this).data('source');
 
-    window.scrollTo(0, 0);
-    $(dest + " section").fadeIn(300);
-    // except main page
-    if(source != ".background-container")
-        $(source + " section").fadeOut(300);
+        //Set the body class
+        $("body").removeClass(source + "-body").addClass(dest + "-body");
 
-    $(sourceMeta).fadeOut(200);
-    $(destMeta).fadeIn(1500);
+        dest = "." + dest;
+        source = "." + source;
+        var destMeta = dest + "-meta";
+        var sourceMeta = source + "-meta";
 
-    // hide the list of projects
-    $(".side-bar").hide();
-    $(".list-of-projects").hide();
+        window.scrollTo(0, 0);
+        $(dest + " section").fadeIn(300);
+        // except main page
+        if(source != ".background-container")
+            $(source + " section").fadeOut(300);
 
-    if ($(dest).hasClass('off-scene-left')) {
-        $(source).removeClass('on-scene');
-        $(dest).removeClass('off-scene-left');
-        $(source).addClass('off-scene-right');
+        $(sourceMeta).fadeOut(200);
+        $(destMeta).fadeIn(1500);
 
-        $(dest).removeClass('off-scene-right');
-        $(dest).removeClass('off-scene-left');
+        // hide the list of projects
+        $(".side-bar").hide();
+        $(".list-of-projects").hide();
 
-        $(dest).addClass('on-scene');
-    } else if ($(dest).hasClass('off-scene-right')) {
-        $(source).removeClass('on-scene');
-        $(dest).removeClass('off-scene-right');
-        $(source).addClass('off-scene-left');
+        if ($(dest).hasClass('off-scene-left')) {
+            $(source).removeClass('on-scene');
+            $(dest).removeClass('off-scene-left');
+            $(source).addClass('off-scene-right');
 
-        $(dest).removeClass('off-scene-right');
-        $(dest).removeClass('off-scene-left');
+            $(dest).removeClass('off-scene-right');
+            $(dest).removeClass('off-scene-left');
 
-        $(dest).addClass('on-scene');
-    }
+            $(dest).addClass('on-scene');
+        } else if ($(dest).hasClass('off-scene-right')) {
+            $(source).removeClass('on-scene');
+            $(dest).removeClass('off-scene-right');
+            $(source).addClass('off-scene-left');
 
-    setTimeout(function(){
-        $(".side-bar").show();
-        $(".list-of-projects").fadeIn(300);
-    }, 500);
-});
+            $(dest).removeClass('off-scene-right');
+            $(dest).removeClass('off-scene-left');
 
-$('[data-jump]').click(function(event) {
-    console.log("Redirection");
-    var dest = $(this).data('destination');
-    var source = $(this).data('source');
+            $(dest).addClass('on-scene');
+        }
 
-    //Set the body class
-    $("body").removeClass(source + "-body").addClass(dest + "-body");
+        setTimeout(function(){
+            $(".side-bar").show();
+            $(".list-of-projects").fadeIn(300);
+        }, 500);
+    });
 
-    dest = "." + dest;
-    source = "." + source;
-    var destMeta = dest + "-meta";
-    var sourceMeta = source + "-meta";
+    $('[data-jump]').click(function(event) {
+        console.log("Redirection");
+        var dest = $(this).data('destination');
+        var source = $(this).data('source');
 
-    window.scrollTo(0, 0);
-    $(dest + " section").fadeIn(300);
-    // except main page
-    if(source != ".background-container")
-        $(source + " section").fadeOut(300);
+        //Set the body class
+        $("body").removeClass(source + "-body").addClass(dest + "-body");
+
+        dest = "." + dest;
+        source = "." + source;
+        var destMeta = dest + "-meta";
+        var sourceMeta = source + "-meta";
+
+        window.scrollTo(0, 0);
+        $(dest + " section").fadeIn(300);
+        // except main page
+        if(source != ".background-container")
+            $(source + " section").fadeOut(300);
 
 
-    $(sourceMeta).fadeOut(200);
-    $(destMeta).fadeIn(1500);
+        $(sourceMeta).fadeOut(200);
+        $(destMeta).fadeIn(1500);
 
-    if (dest == '.works-page') {
-        $(source).removeClass('on-scene').removeClass('off-scene-right').addClass('off-scene-left');
-        $(dest).removeClass('off-scene-right').removeClass('off-scene-left').addClass('on-scene');
-        $(".background-container").removeClass('off-scene-right').hide().addClass('off-scene-left').show(400);
-    } else if (dest == '.about-page') {
-        $(source).removeClass('on-scene').removeClass('off-scene-left').addClass('off-scene-right');
-        $(dest).removeClass('off-scene-right').removeClass('off-scene-left').addClass('on-scene');
-        $(".background-container").removeClass('off-scene-left').hide().addClass('off-scene-right').show(400);
+        if (dest == '.works-page') {
+            $(source).removeClass('on-scene').removeClass('off-scene-right').addClass('off-scene-left');
+            $(dest).removeClass('off-scene-right').removeClass('off-scene-left').addClass('on-scene');
+            $(".background-container").removeClass('off-scene-right').hide().addClass('off-scene-left').show(400);
+        } else if (dest == '.about-page') {
+            $(source).removeClass('on-scene').removeClass('off-scene-left').addClass('off-scene-right');
+            $(dest).removeClass('off-scene-right').removeClass('off-scene-left').addClass('on-scene');
+            $(".background-container").removeClass('off-scene-left').hide().addClass('off-scene-right').show(400);
 
-    }
-});
+        }
+    });
 
-$(".side-bar").click(function(){
-    //Disable work page scrolling
-    $.fn.fullpage.setAllowScrolling(false);
+    $(".side-bar").click(function(){
+        //Disable work page scrolling
+        $.fn.fullpage.setAllowScrolling(false);
 
-    $("body").addClass("opensidebar");
-    $(".side-bar-overlay").fadeIn(400, function(){
-        $(".list-of-projects").addClass("close");
+        $("body").addClass("opensidebar");
+        $(".side-bar-overlay").fadeIn(400, function(){
+            $(".list-of-projects").addClass("close");
+        });
+    });
+
+    $('.parallex-item').parallax();
+
+    $('[data-view-process]').click(function(event) {
+        var dest = $(this).data('detail-page');
+        loadTemplate(dest);
+        $("body").addClass("detail-page-open");
+        //Enable work page scrolling
+        $.fn.fullpage.setAllowScrolling(false);
+        $(".detail-page").fadeIn();
+        $(".logo-light").fadeOut(function(){
+            $(".visible-detail-page").fadeIn();
+        });
+
+    });
+
+    $(".back-to-home.visible-detail-page").click(function(){
+        $(".visible-detail-page").fadeOut(function(){
+            $(".logo-light").fadeIn();
+        });
+        $.fn.fullpage.setAllowScrolling(true);
+        $(".detail-page").fadeOut();
+        $("body").removeClass("detail-page-open");
     });
 });
+
 
 $(document).on('click', '.list-of-projects.close .circle', function()
 {
@@ -112,7 +154,12 @@ $(document).on('click', '.list-of-projects.close .circle', function()
 
 });
 
-$('[data-view-process]').click(function(event) {
-    var dest = $(this).data('detail-page');
-    $(".detail-page").fadeIn();
-});
+function loadTemplate(temp) {
+    var templateURL     = 'tpl/' + temp + ".html",
+        $container = $('<div></div>');
+    var param = {};
+
+    $container.load(templateURL, param, function(data, status, xhr) {
+        $(".detail-page").html(data);
+    });
+}
